17ad02fbd1423f4e2e77168ea1fdbc2cd45911fa
Miguel Ángel Torres Vargas <ingeniero.miguelvargas@gmail.com>
2020-10-01 21:12:39 -0500

    Init project

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <v-main>
+      <nuxt />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  mounted () {
+    this.$store.dispatch('generateAccessToken')
+    if (process.browser) {
+      if (typeof localStorage.getItem('authToken') === 'undefined') {
+        this.$cookie.removeAll()
+        this.$store.dispatch('cleanAll')
+      }
+      this.$store.dispatch('generateAccessToken')
+    }
+  }
+}
+</script>
+<style>
+.v-btn__content {
+    text-transform: none !important;
+    letter-spacing: normal !important;
+}
+</style>
